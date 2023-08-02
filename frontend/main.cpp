ea611a3043688a7867628267de6a3ebbd5df490d
xsqian <1757949943@qq.com>
2023-08-02 06:29:00 +0000

    add malicious model

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -22,20 +22,110 @@
 #include "tools.h"
 #include "volePSI/Paxos.h"
 #include "coproto/Socket/AsioSocket.h"
+#include "miniMPSI_re.h"
 #include "miniMPSI.h"
 using namespace osuCrypto;
 using namespace volePSI;
+
 void PrintParamInfo(u64 nParties, u64 setSize, u64 SecParam, u64 StaParam, bool malicious)
 {
 
-  std::cout << std::left << std::setw(15) << "参与方数量：" << nParties << std::endl;
-  std::cout << std::left << std::setw(15) << "集合大小：" << setSize << std::endl;
-  std::cout << std::left << std::setw(15) << "计算安全参数：" << SecParam << std::endl;
-  std::cout << std::left << std::setw(15) << "统计安全参数：" << StaParam << std::endl;
-  std::cout << std::left << std::setw(15) << "是否恶意安全：" << (malicious == 1 ? "是" : "否") << std::endl;
+  std::cout << oc::Color::Red
+            << "number of parties: " << nParties << std::endl
+            << "set size: " << setSize << std::endl
+            << "computational security parameters: " << SecParam << std::endl
+            << "statistical security parameters: " << StaParam << std::endl
+            << "malicious model? " << (malicious == 1 ? "yes" : "no") << oc::Color::Default << std::endl;
+}
+// 多终端执行代码
+void party(u64 nParties, u64 setSize, u64 myIdx, u64 num_Threads, bool malicious, u64 flag)
+{
+  // 初始化计算安全参数和 统计安全参数
+  u64 SecParam = 180,
+      StaParam = 40;
+  if (flag == 1 || (flag == 0 && myIdx == 0))
+    PrintParamInfo(nParties, setSize, SecParam, StaParam, malicious);
+  u64 expectedIntersection = 200;
+  std::vector<oc::Socket> chls(nParties);
+  for (u64 i = 0; i < nParties; ++i)
+  {
+    if (i < myIdx)
+    {
+      u32 port = 1200 + i * 100 + myIdx;
+      std::string ip = "localhost:" + std::to_string(port);
+      // std::cout << "ip: " << ip << std::endl;
+      chls[i] = coproto::asioConnect(ip, 0);
+    }
+    else if (i > myIdx)
+    {
+      u32 port = 1200 + myIdx * 100 + i; // get the same port; i=2 & pIdx=1 =>port=102
+                                         // chls[i].resize(numThreads);
+      std::string ip = "localhost:" + std::to_string(port);
+      // std::cout << "ip: " << ip << std::endl;
+      chls[i] = coproto::asioConnect(ip, 1);
+    }
+  }
+
+  // 首先生成 零共享值
+  std::vector<PRNG> mPrngs(nParties);
+  std::mutex mtx;
+  PRNG prngSet(_mm_set_epi32(4253465, 3434565, 234435, 0));
+  PRNG prng0(_mm_set_epi32(4253465, 3434565, 234435, 23987045));
+  PRNG prng1(_mm_set_epi32(4253465, 3434565, 234435, 23987025));
+  std::vector<std::vector<block>> zsSeeds(nParties); // 存储随机种子
+
+  // 为每个用户生成nparties个随机种子
+  for (u64 i = 0; i < nParties; i++)
+  {
+    zsSeeds[i].resize(nParties);
+    for (u64 j = 0; j < nParties; j++)
+    {
+      if (i <= j)
+      {
+        zsSeeds[i][j] = prng0.get<block>();
+      }
+      else
+        zsSeeds[i][j] = zsSeeds[j][i];
+    }
+  }
+  mPrngs.resize(nParties);
+  for (u64 i = 0; i < nParties; i++)
+  {
+    mPrngs[i].SetSeed(zsSeeds[myIdx][i]);
+  }
+  if (myIdx == 0)
+  {
+    // 创建集合
+    std::vector<block> inputs(setSize);
+    for (u64 i = 1; i < expectedIntersection + 1; i++)
+      inputs[i] = prngSet.get<block>();
+    prng1.SetSeed(block(myIdx, myIdx));
+    for (u64 i = expectedIntersection + 1; i < setSize; i++)
+      inputs[i] = prng1.get<block>();
+    inputs[0] = prng1.get<block>();
+    volePSI::miniMPSIReceiver_re receiver;
+    receiver.init(128, 40, nParties, myIdx, setSize, inputs, true, num_Threads);
+    std::vector<block> ans = (receiver.receive(mPrngs, chls, num_Threads));
+    std::cout << "instersection size is " << ans.size() << std::endl;
+  }
+  else
+  {
+
+    std::vector<block> inputs(setSize);
+    for (u64 i = 1; i < expectedIntersection + 1; i++)
+      inputs[i] = prngSet.get<block>();
+    prng1.SetSeed(block(myIdx, myIdx));
+    for (u64 i = expectedIntersection + 1; i < setSize; i++)
+      inputs[i] = prng1.get<block>();
+    inputs[0] = prng1.get<block>();
+    volePSI::miniMPSISender_re sender;
+    sender.init(128, 40, nParties, myIdx, setSize, inputs, true, num_Threads);
+    (sender.send(mPrngs, chls, num_Threads));
+  }
 }
 void tparty(u64 nParties, u64 setSize, u64 myIdx, u64 num_Threads)
 {
+
   // 初始化计算安全参数和 统计安全参数
   u64 SecParam = 180,
       StaParam = 40;
@@ -163,10 +253,10 @@ void tparty(u64 nParties, u64 setSize, u64 myIdx, u64 num_Threads)
   }
 }
 
-// #include "volepsi/volePSI/Paxos.h"
 void PrintInfo()
 {
-  std::cout << oc::Color::Green << "#####################################################################\n"
+  std::cout << oc::Color::Green
+            << "#####################################################################\n"
             << "#####################################################################\n"
             << "#                                                                   #\n"
             << "#                             miniMSPI                              #\n"
@@ -178,16 +268,20 @@ void PrintInfo()
             << "   -n: number of parties.\n"
             << "   -m: input set size ( 2^m ).\n"
             << "   -mm: input set size ( mm ).\n"
-            << "   -p: the party id (must be a continuous integer of 1-( n-1 ) ).\n"
+            << "   -p: the party ID (must be a continuous integer of 1-( n-1 ) ).\n"
             << "   -t: number of threads.\n"
-            << "   -u: Run unit test.\n\n";
+            << "   -r: 0 is semihonest model, 1 is malicous model.\n"
+            << "   -u: Run unit test.\n\n"
+            << oc::Color::Default;
 }
+
 int main(int argc, char **argv)
 {
   u64 nParties, setSize, pIdx, numthreads;
+  bool malicious = false;
   switch (argc)
   {
-  case 9:
+  case 11:
     if (argv[1][0] == '-' && argv[1][1] == 'n')
     {
       nParties = atoi(argv[2]);
@@ -213,24 +307,33 @@ int main(int argc, char **argv)
     if (argv[7][0] == '-' && argv[7][1] == 't')
     {
       numthreads = atoi(argv[8]);
-      tparty(nParties, setSize, pIdx, numthreads);
     }
     else
       PrintInfo();
-
+    if (strcmp(argv[9], "-r") == 0)
+    {
+      if (strcmp(argv[10], "1") == 0)
+        malicious = true;
+      else
+        malicious = false;
+      party(nParties, setSize, pIdx, numthreads, malicious, 1);
+    }
+    else
+      PrintInfo();
     break;
   case 2:
     if (argv[1][0] == '-' && argv[1][1] == 'u')
     {
-      nParties = 32;
+      nParties = 3;
       setSize = 1 << 9;
       numthreads = 6;
+
       std::vector<std::thread> pThrds(nParties);
       for (u64 pIdx = 0; pIdx < pThrds.size(); ++pIdx)
       {
         pThrds[pIdx] =
             std::thread([&, pIdx]()
-                        { tparty(nParties, setSize, pIdx, numthreads); });
+                        { party(nParties, setSize, pIdx, numthreads, malicious, 0); });
       }
       for (u64 pIdx = 0; pIdx < pThrds.size(); ++pIdx)
         pThrds[pIdx].join();
@@ -242,160 +345,3 @@ int main(int argc, char **argv)
   }
   return 0;
 }
-
-/* int main(int argc, char** argv)
-{
-    oc::CLP cmd(argc, argv);
-
-    //std::ofstream file("./set.csv");
-    //for (oc::u64 i = 0; i < 10000000; ++i)
-    //    file << std::setfill('0') << std::setw(32) << i << "\n";
-    //return 0;
-    if (cmd.isSet("in"))
-    {
-        volePSI::doFilePSI(cmd);
-    }
-    else if (cmd.isSet("messagePassing"))
-    {
-        messagePassingExample(cmd);
-    }
-    else if (cmd.isSet("net"))
-    {
-        networkSocketExample(cmd);
-    }
-    else if (cmd.isSet("exp"))
-    {
-        Paxos_experiment(cmd);
-    }
-    else if (cmd.isSet("perf"))
-    {
-        perf(cmd);
-    }
-    else if (cmd.isSet("balls"))
-    {
-        overflow(cmd);
-    }
-    else if(cmd.isSet("u"))
-    {
-        auto r = volePSI_Tests::Tests.runIf(cmd);
-        return r == oc::TestCollection::Result::failed;
-    }
-    else
-    {
-
-        std::cout << oc::Color::Green << "File based PSI Parameters:\n" <<
-oc::Color::Default
-            << "   -in <value>: The path to the party's set. Should either be a
-binary file containing 16 byte elements with a .bin extension. "
-            << "Otherwise the path should have a .csv extension and have one
-element per row, 32 char hex rows are preferred. \n"
-
-            << "   -r <value>: value should be in { 0, 1 } where 0 means PSI
-sender.\n"
-
-            << "   -out <value>: The output file path. Will be written in the
-same format as the input. (Default = in || \".out\")\n"
-            << "   -quiet: print less info.\n"
-            << "   -v: print more info.\n"
-            << "   -indexSet: output the index set of the intersection instead
-of the set element itself\n"
-            << "   -noSort: do not require the output to be in the same order
-and the input (slightly faster)."
-            << "   -malicious: run the protocol with malicious security\n"
-            << "   -useSilver: run the protocol with the Silver Vole encoder
-(experimental, default is expand accumulate)\n"
-            << "   -useQC: run the protocol with the QuasiCyclic Vole encoder
-(default is expand accumulate)\n"
-            << "   -ssp: Statistical Security parameter, default = 40.\n\n"
-
-            << "   -ip <value>: IP address and port of the server = PSI
-receiver. (Default = localhost:1212)\n"
-            << "   -server <value>: Value should be in {0, 1} and indicates if
-this party should be the IP server. (Default = r)\n"
-            << "   -tls: run the protocol with TLS. Must also set -CA,-sk,-pk\n"
-            << "   -CA <value>: if tls, then this must be the path to the CA
-cert file in pem format\n"
-            << "   -pk <value>: if tls, then this must be the path to this
-parties public key cert file in pem format\n"
-            << "   -sk <value>: if tls, then this must be the path to this
-parties private key file in pem format\n\n"
-
-            << "   -bin: Optional flag to always interpret the input file as
-binary.\n"
-            << "   -csv: Optional flag to always interpret the input file as a
-CSV.\n"
-            << "   -receiverSize <value>: An optional parameter to specify the
-receiver's set size.\n"
-            << "   -senderSize <value>: An optional parameter to specify the
-sender's set size.\n\n"
-
-            ;
-
-
-        std::cout << oc::Color::Green << "Example programs: \n" <<
-oc::Color::Default
-            << "   -messagePassing: Runs the message passing example program.
-This example shows how to manually pass messages between the PSI parties. Same
-parameters as File base PSI can be used.\n"
-            << "   -net: Run the network socket (TCP/IP or TLS) example program.
-This example shows how to run the protocol on the coproto network socket. Same
-parameters as File base PSI can be used.\n\n"
-
-            ;
-
-
-        std::cout << oc::Color::Green << "Benchmark programs: \n" <<
-oc::Color::Default
-            << "   -perf: required flag to run benchmarking\n"
-            << "   -psi: Run the PSI benchmark.\n"
-            << "      -nn <value>: the log2 size of the sets.\n"
-            << "      -t <value>: the number of trials.\n"
-            << "      -malicious: run with malicious security.\n"
-            << "      -v: verbose.\n"
-            << "      -nt: number of threads.\n"
-            << "      -fakeBase: use fake base OTs.\n"
-            << "      -nc: do not compress the OPRF outputs.\n"
-            << "      -useSilver: run the protocol with the Silver Vole encoder
-(experimental, default is expand accumulate)\n"
-            << "      -useQC: run the protocol with the QuasiCyclic Vole encoder
-(default is expand accumulate)\n"
-            << "      -bs: the okvs bin size.\n"
-            << "   -cpsi: Run the circuit psi benchmark.\n"
-            << "      -nn <value>: the log2 size of the sets.\n"
-            << "      -t <value>: the number of trials.\n"
-            << "      -v: verbose.\n"
-            << "      -nt: number of threads.\n"
-            << "   -paxos: Run the okvs benchmark.\n"
-            << "      -n <value>: The set size. Can also set n using -nn wher
-n=2^nn.\n"
-            << "      -t <value>: the number of trials.\n"
-            << "      -b <value>: The bitcount of the index type. Must by a
-multiple of 8 and greater than 1.3*n.\n"
-            << "      -v: verbose.\n"
-            << "      -w <value>: The okvs weight.\n"
-            << "      -ssp <value>: statistical security parameter.\n"
-            << "      -binary: binary okvs dense columns.\n"
-            << "      -cols: The size of the okvs elemenst in multiples of 16
-bytes. default = 1.\n"
-            << "   -baxos: The the bin okvs benchmark. Same parameters as -paxos
-plus.\n"
-            << "      -lbs <value>: the log2 bin size.\n"
-            << "      -nt: number of threads.\n"
-
-            ;
-
-
-
-
-
-        std::cout << oc::Color::Green << "Unit tests: \n" << oc::Color::Default
-            << "   -u: Run all of the unit tests.\n"
-            << "   -u -list: List run all of the unit tests.\n"
-            << "   -u 10 15: Run unit test 10 and 15.\n"
-            << "   -u 10..15: Run unit test 10 to 15 (exclusive).\n"
-            << "   -u psi: Run unit test that contain \"psi\" is the title.\n\n"
-            ;
-    }
-
-    return 0;
-} */
---
+++ frontend/miniMPSI_re.cpp
@@ -0,0 +1,339 @@
+#include "miniMPSI_re.h"
+
+namespace volePSI
+{
+    void miniMPSISender_re::init(u64 secParam, u64 stasecParam, u64 nParties, u64 myIdx, u64 setSize, std::vector<block> inputs, bool malicious, u64 numThreads)
+    {
+        this->secParam = secParam;
+        this->stasecParam = stasecParam;
+        this->nParties = nParties;
+        this->myIdx = myIdx;
+        this->setSize = setSize;
+        this->inputs = inputs;
+        this->malicious = malicious;
+        this->numThreads = numThreads;
+    }
+
+    void miniMPSISender_re::send(std::vector<PRNG> &mseed, std::vector<Socket> &chl, u64 numThreads)
+    {
+        // define variables
+        std::vector<u8> zeroValue(nParties);
+        REllipticCurve mCrurve;
+        REllipticCurve::Point mG;
+        PRNG prng;
+        oc::Matrix<u8> val;
+        oc::Matrix<u8> pax;
+        oc::Matrix<u8> pax2;
+        size_t len = 0;
+        size_t size = 0;
+        std::vector<REccPoint> px(setSize);
+        std::vector<REccPoint> npx(setSize);
+        std::vector<std::vector<u8>> values(setSize);
+        std::vector<std::thread> thrds(nParties);
+        Socket fork;
+        // if malicious mode is enabled
+        if (malicious == true)
+        {
+            oc::RandomOracle hash(sizeof(block));
+            // #pragma omp parallel for num_threads(numThreads)
+            for (u64 i = 0; i < inputs.size(); i++)
+            {
+                hash.Reset();
+                hash.Update(inputs[i]);
+                block hh;
+                hash.Final(hh);
+                inputs[i] = hh;
+            }
+        }
+        timer.setTimePoint("miniMPSI::sender " + std::to_string(myIdx) + " start");
+        // create zeroshare values
+        zeroValue[0] = 0;
+#pragma omp parallel for num_threads(numThreads)
+        for (u64 i = 0; i < nParties; i++)
+        {
+            if (i != myIdx)
+                zeroValue[i] = zeroValue[i] ^ mseed[i].get<u8>();
+        }
+        // choice a random number a_i and compute g^(a_i) send it to the server
+        prng.SetSeed(toBlock(myIdx, myIdx));
+        mG = mCrurve.getGenerator();
+        ai.randomize(prng);
+        g_ai = mG * ai;
+        std::vector<u8> points = REccPoint_to_Vector(g_ai);
+        macoro::sync_wait(chl[0].send(coproto::copy(points)));
+
+        // receive parameters of OKVS result vector
+        // first parameter vector rows size
+        macoro::sync_wait(chl[0].recv(size));
+        // second parameter vector column size
+        macoro::sync_wait(chl[0].recv(len));
+        pax.resize(size, len), val.resize(setSize, len);
+        // receive vector of OKVS result
+        macoro::sync_wait(chl[0].recv(pax));
+        paxos.init(setSize, 128, 3, stasecParam, PaxosParam::Binary, block(0, 0));
+        // OKVS Decode for parties inputs value
+        paxos.decode<u8>(inputs, val, pax, numThreads);
+        // compute g_(a_i*b_i)
+        // #pragma omp parallel for num_threads(numThreads)
+        for (u64 i = 0; i < setSize; i++)
+        {
+            std::vector<u8> vec(len);
+            // #pragma omp parallel for num_threads(numThreads)
+            for (u64 j = 0; j < len; j++)
+                vec[j] = val[i][j];
+            px.emplace_back(mCrurve);
+            px[i] = vector_to_REccPoint(vec);
+            npx.emplace_back(mCrurve);
+            // compute g_(a_i*b_i)
+            npx[i] = px[i] * ai;
+            // XOR calculation results with zero shared values
+            npx[i] = REccPoint_xor_u8(npx[i], zeroValue);
+        }
+        // OKVS encode for (inouts and g^(a_i*b_i) \xor (zeroshare values))
+        paxos.init(setSize, 128, 3, stasecParam, PaxosParam::Binary, block(0, 0));
+        pax2.resize(paxos.size(), len);
+        // #pragma omp parallel for num_threads(6)
+        for (u64 i = 0; i < setSize; i++)
+        {
+            values[i] = REccPoint_to_Vector(npx[i]);
+            // #pragma omp parallel for num_threads(6)
+
+            for (size_t j = 0; j < len; j++)
+                val(i, j) = values[i][j];
+        }
+        paxos.solve<u8>(inputs, val, pax2, &prng, numThreads);
+        // Sending paxos.size() directly will block if it doesn't work. I don't know why
+        size = paxos.size();
+        macoro::sync_wait(chl[0].send(size));
+        macoro::sync_wait(chl[0].send(coproto::copy(pax2)));
+        timer.setTimePoint("miniMPSI::sender " + std::to_string(myIdx) + " end");
+
+        std::cout << timer << std::endl;
+        macoro::sync_wait(macoro::suspend_always{});
+        for (u64 i = 0; i < chl.size(); i++)
+        {
+
+            if (i != myIdx)
+            {
+                (chl[i].flush());
+                chl[i].close();
+            }
+        }
+        return;
+    };
+    std::vector<block> miniMPSIReceiver_re::receive(std::vector<PRNG> &mseed, std::vector<Socket> &chl, u64 numThreads)
+    {
+        // define variables
+        std::vector<u8> zeroValue(nParties);
+        REllipticCurve mCrurve;
+        REllipticCurve::Point mG;
+        PRNG prng;
+        Socket fork;
+        REccPoint tempPoint;
+        std::vector<REccPoint> akrandom(nParties);
+        std::vector<REccNumber> nSeeds(nParties);
+        std::vector<REccPoint> mypoint(nParties);
+        std::vector<std::vector<u8>> values(setSize);
+        oc::Matrix<u8> val;
+        oc::Matrix<u8> val2;
+        oc::Matrix<u8> pax;
+        size_t len = 0;
+        u64 size = 0;
+        std::vector<oc::Matrix<u8>> allval;
+        std::vector<REccPoint> px(setSize);
+        std::vector<REccPoint> npx(setSize);
+        std::vector<std::vector<REccPoint>> allpx(nParties);
+        std::vector<u8> intersection_bit;
+        std::vector<REccPoint> allkey;
+        u64 intersection_size = 0;
+        std::vector<std::thread> thrds(nParties);
+        timer.setTimePoint("miniMPSI::reciver start");
+        std::vector<block> reinputs(setSize); // save original input
+        reinputs = inputs;
+        // if malicious mode is enabled
+
+        if (malicious == true)
+        {
+            oc::RandomOracle hash(sizeof(block));
+            // #pragma omp parallel for num_threads(numThreads)
+
+            for (u64 i = 0; i < inputs.size(); i++)
+            {
+                hash.Reset();
+                hash.Update(inputs[i]);
+                block hh;
+                hash.Final(hh);
+                inputs[i] = hh;
+            }
+        }
+        // create zeroshare values
+
+        zeroValue[0] = 0;
+#pragma omp parallel for num_threads(numThreads)
+
+        for (u64 i = 1; i < nParties; i++)
+        {
+            zeroValue[i] = zeroValue[i] ^ mseed[i].get<u8>();
+        }
+
+        // receive g_ai values from other parties
+        prng.SetSeed(toBlock(myIdx, myIdx));
+        mG = mCrurve.getGenerator();
+        // ai.randomize(prng);
+        // g_ai = mG * ai;
+        // akrandom.resize(nParties);
+        // akrandom.emplace_back(mCrurve);
+        // akrandom[0] = g_ai;
+        for (u64 i = 1; i < nParties; i++)
+        {
+            tempPoint = mCrurve;
+            std::vector<u8> points(g_ai.sizeBytes());
+            macoro::sync_wait(chl[i].recv((points)));
+            tempPoint.fromBytes(points.data());
+            akrandom.emplace_back(mCrurve);
+            akrandom[i] = tempPoint;
+        }
+        // Create collection sized elliptical curve points
+        for (u64 i = 0; i < setSize; i++)
+        {
+            nSeeds.emplace_back(mCrurve);
+            nSeeds[i].randomize(prng);
+            mypoint.emplace_back(mCrurve);
+            mypoint[i] = mG * nSeeds[i]; // g^ri
+            values[i] = REccPoint_to_Vector(mypoint[i]);
+        }
+
+        len = values[0].size();
+        val.resize(setSize, len);
+#pragma omp parallel for num_threads(numThreads)
+
+        for (u64 i = 0; i < setSize; i++)
+        {
+            // Make sure the size of the vector in values[i] matches the number of columns 'n'
+            COPROTO_ASSERT(values[i].size() == len);
+            // Copy each element from the vector in values[i] to the corresponding row in the 'val' matrix
+#pragma omp parallel for num_threads(numThreads)
+            for (size_t j = 0; j < len; j++)
+            {
+                val(i, j) = values[i][j];
+            }
+        }
+
+        //  OKVS encode for (inputs, g_(a_i))
+        paxos.init(setSize, 128, 3, stasecParam, PaxosParam::Binary, block(0, 0));
+        pax.resize(paxos.size(), len);
+        paxos.solve<u8>(inputs, val, pax, &prng, numThreads);
+        // send parameters of OKVS encode results
+#pragma omp parallel for num_threads(numThreads)
+        for (u64 i = 1; i < nParties; i++)
+        {
+            macoro::sync_wait(chl[i].send(paxos.size()));
+            macoro::sync_wait(chl[i].send(len));
+            macoro::sync_wait(chl[i].send(coproto::copy(pax)));
+        }
+
+        /*
+            Homomorphism of XOR operations
+            a^b^c=(a^c)^(b^c)
+         */
+        // #pragma omp parallel for num_threads(numThreads)
+        for (u64 i = 1; i < nParties; i++)
+        {
+            size_t size = 0;
+            macoro::sync_wait(chl[i].recv(size));
+            oc::Matrix<u8> pax2(size, len);
+            macoro::sync_wait(chl[i].recv(pax2));
+            oc::Matrix<u8> val3(setSize, len);
+            paxos.decode<u8>(inputs, val3, pax2, numThreads);
+            // Convert a vector to ReccPoint and perform XOR operation
+            allpx[i].resize(setSize);
+            // #pragma omp parallel for num_threads(numThreads)
+            for (u64 k = 0; k < val3.rows(); k++)
+            {
+                std::vector<u8> tem(val3.cols());
+                for (u64 j = 0; j < val3.cols(); j++)
+                {
+                    tem[j] = val3[k][j];
+                }
+                allpx[i][k] = vector_to_REccPoint(tem);
+            }
+        }
+        // The final result of the XOR operation is saved in the second row of allpx
+        // #pragma omp parallel for num_threads(numThreads)
+
+        for (u64 j = 0; j < allpx[1].size(); j++)
+        {
+            // #pragma omp parallel for num_threads(numThreads)
+
+            for (u64 i = 2; i < allpx.size(); i++)
+            {
+                allpx[1][j] = allpx[1][j] + allpx[i][j];
+            }
+        }
+
+        // Insert the XOR operation result into the bloom filter
+        Filter.init(setSize, stasecParam);
+        for (u64 i = 0; i < setSize; i++)
+        {
+            allpx[1][i] = REccPoint_xor_u8(allpx[1][i], zeroValue);
+            Filter.Insert(allpx[1][i]);
+        }
+        Filter.PrintInfo();
+
+        // create the key set for leader
+        for (u64 i = 0; i < setSize; i++)
+        {
+            std::vector<REccPoint> userkey(nParties);
+            userkey.emplace_back(mCrurve);
+            for (u64 j = 1; j < nParties; j++)
+            {
+                userkey.emplace_back(mCrurve);
+                userkey[j] = (akrandom[j] * nSeeds[i]);
+            }
+            // xor operation
+            for (u64 k = 2; k < nParties; k++)
+            {
+                userkey[1] = userkey[1] + userkey[k];
+            }
+            allkey.push_back(userkey[1]);
+        }
+        // compute the intersection
+        intersection_bit = Filter.Contain(allkey);
+        // #pragma omp parallel for num_threads(numThreads)
+
+        for (u64 i = 0; i < intersection_bit.size(); i++)
+        {
+            if (intersection_bit[i] == 1)
+                outputs.push_back(reinputs[i]);
+        }
+        timer.setTimePoint("miniMPSI::reciver end");
+
+        std::cout << "intersection size: " << outputs.size() << std::endl;
+        std::cout << timer << std::endl;
+        // for (u64 i = 0; i < outputs.size(); i++)
+        // {
+        //     std::cout << outputs[i] << std::endl;
+        // }
+        // macoro::sync_wait(macoro::suspend_always{});
+        // for (u64 i = 0; i < chl.size(); i++)
+        // {
+        //     if (i != myIdx)
+        //     {
+        //         (chl[i].flush());
+        //         chl[i].close();
+        //     }
+        // }
+        return outputs;
+    };
+    void miniMPSIReceiver_re::init(u64 secParam, u64 stasecParam, u64 nParties, u64 myIdx, u64 setSize, std::vector<block> inputs, bool malicious, u64 numThreads)
+    {
+        this->secParam = secParam;
+        this->stasecParam = stasecParam;
+        this->nParties = nParties;
+        this->myIdx = myIdx;
+        this->setSize = setSize;
+        this->inputs = inputs;
+        this->malicious = malicious;
+        this->numThreads = numThreads;
+    }
+}
